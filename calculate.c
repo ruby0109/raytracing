3e176f6fa3c95921a7368c9de04c6205dd9b031e
ruby0109 <ruby620412@gmail.com>
2016-03-22 06:30:02 +0000

    Added files via upload

---
+++ calculate.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fp = fopen("orig.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+    if (!fp) {
+        printf("ERROR opening input file orig.txt\n");
+        exit(0);
+    }
+    int i = 0;
+    char exTime[50];
+    double orig_sum_t = 0.0,  orig_t;
+    for (i = 0; i < 100; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        fscanf(fp, "%s %lf\n", exTime,&orig_t);
+        orig_sum_t += orig_t;
+    }
+    fclose(fp);
+
+    fp = fopen("opt.txt", "r");
+    if (!fp) {
+        fp = fopen("orig.txt", "r");
+        if (!fp) {
+            printf("ERROR opening input file opt.txt\n");
+            exit(0);
+        }
+    }
+    double opt_sum_t = 0.0, opt_t;
+    for (i = 0; i < 100; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        fscanf(fp, "%s %lf\n", exTime,&opt_t);
+        opt_sum_t += opt_t;
+    }
+    fprintf(output, "ExecutionTime: %lf %lf\n",orig_sum_t / 100.0, opt_sum_t / 100.0);
+    fclose(output);
+    fclose(fp);
+    return 0;
+}
--- math-toolkit_opt.h
+++ math-toolkit_opt.h
@@ -4,8 +4,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
-#include <stdint.h>
 #include <immintrin.h>
+#include <pmmintrin.h>
 
 #define OPT 1
 
@@ -36,28 +36,15 @@ void add_vector(const double *a, const double *b, double *out)
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    __m256 out = 0.0;
-    __m256 A = _mm_load_ps(a);
-    __m256 B = _mm_load_ps(b);
-
-    out = _mm_sub_ps( A, B);
-
-    /*
     for (int i = 0; i < 3; i++)
-        out[i] = a[i] - b[i];*/
+        out[i] = a[i] - b[i];
 }
 
 static inline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
-    __m256 out = 0.0;
-    __m256 A1 = _mm_load_ps(a);
-    __m256 B1 = _mm_load_ps(b);
-
-    out = _mm_mult_ps( A1, B1);
-    /*
     for (int i = 0; i < 3; i++)
-        out[i] = a[i] * b[i];*/
+        out[i] = a[i] * b[i];
 }
 
 static inline
@@ -70,47 +57,30 @@ void multiply_vector(const double *a, double b, double *out)
 static inline
 void cross_product(const double *v1, const double *v2, double *out)
 {
-    /*
-    __m256 out = 0.0;
-    __m256 V1 = _mm_load_ps(v1);
-    __m256 V2 = _mm_load_ps(v2);
-
-    const uint32_t YZXMask = _MM_SHUFFLE( 1, 2, 0, 0 );
-    const uint32_t ZXYMask = _MM_SHUFFLE( 2, 0, 1, 0 );
-    
-    __m256 cpTemp1 = _mm_shuffle_ps( V1, V1, YZXMask);
-    __m256 cpTemp2 = _mm_shuffle_ps( V2, V2, ZXYMask);
-    __m256 cpMult1 = _mm_mult_ps( cpTemp1, cpTemp2);
-    
-    cpTemp1 = _mm_shuffle_ps( V1, V1, ZXYMask);
-    cpTemp2 = _mm_shuffle_ps( V2, V2, YZXMask);
-    __m256 cpMult2 = _mm_mult_ps( cpTemp1, cpTemp2);
-    
-    out =  _mm_sub_ps( cpMult1, cpMult2);
-    */
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
     out[1] = v1[2] * v2[0] - v1[0] * v2[2];
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
-
-
 }
 
 static inline
 double dot_product(const double *v1, const double *v2)
 {
-    __m256 dp = 0.0;
-    __m256 V1 = _mm_load_ps(v1);
-    __m256 V2 = _mm_load_ps(v2);
-    
-    __m256 dp += _mm_add_ps(dp, _mm_mul_ps(V1,V2)); 
-    /*_mm128d dp = 0.0;
+    double dp = 0.0;
+    __m256d ymm0 = _mm256_setzero_pd();
 
     for (int i = 0; i < 3; i++)
     {
-        _mm128d x1 = _mmload_pd(v1[i]);
-	_mm128d x2 = _mmload_pd(v2[i]);
+        _mm128d x1 = _mm_load_pd(v1[i]);
+	_mm128d x2 = _mm_load_pd(v2[i]);
         _mm128d dp += _mm_add_pd(dp, _mm_mul_pd(x1,x2);
-    }*/
+    }
+
+    double tmp[4] __attribute__((aligned(32)));
+    _mm256_store_pd(tmp, ymm0);
+    dp += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+    /*double dp = 0.0;
+    for (int i = 0; i < 3; i++)
+        dp += v1[i] * v2[i];*/
     return dp;
 }
 
